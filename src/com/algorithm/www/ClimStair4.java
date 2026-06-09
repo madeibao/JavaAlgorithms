9ae72d6997674e9afda5bfd79eacb5302422a234
madeibao <2901429479@qq.com>
2026-06-09 12:10:26 +0800

    add demo for climStairs

--- src/com/algorithm/www/ClimStair4.java
+++ src/com/algorithm/www/ClimStair4.java
@@ -15,29 +15,27 @@ public class ClimStair4 {
     public static int clim(int n) {
         if(n<=0) {
             return -1;
-        }
-        else {
-            if(n==1) {
+        } else {
+            if (n == 1) {
                 return 1;
-            }
-            else if(n==2) {
+            } else if (n == 2) {
                 return 2;
-            }
-            else{
-                int a= 1;
-                int b= 2;
+            } else {
+                int a = 1;
+                int b = 2;
                 int res = 0;
-                for(int i=2;i<n;i++) {
-                    res = a+b;
-                    a= b;
-                    b =res;
+                for (int i = 2; i < n; i++) {
+                    res = a + b;
+                    a = b;
+                    b = res;
                 }
                 return res;
             }
         }
     }
+
     public static void main(String[] args) {
-        int n =5;
+        int n = 5;
         System.out.println(clim(n));
     }
 }
