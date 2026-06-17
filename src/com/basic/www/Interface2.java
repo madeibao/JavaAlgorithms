f8bb6f26515321ab2ad0a676760e59d9d741d056
madeibao <2901429479@qq.com>
2026-06-17 15:36:43 +0800

    add some demo

--- src/com/basic/www/Interface2.java
+++ src/com/basic/www/Interface2.java
@@ -16,16 +16,11 @@ import java.util.List;
 
  **/
 public class Interface2 {
-
     public static void main(String[] args) {
-        List list=new ArrayList<Integer>(5);
+        List<Integer> list= new ArrayList<>(5);
         list.add(1);
         list.add(2);
         list.add(3);
-
-
         list.forEach(System.out::println);
-
-
     }
 }
