4be9a2e0571fca89c52128ff8874bcec21ab4baf
madeibao <2901429479@qq.com>
2026-07-26 17:07:48 +0800

    add demo for ArrayList

--- src/com/datastructure/www/ArrayListAdd.java
+++ src/com/datastructure/www/ArrayListAdd.java
@@ -14,22 +14,18 @@ import java.util.List;
  * 链表的添加的操作
  **/
 public class ArrayListAdd {
-
     public static void main(String[] args) {
-
         ArrayList<Integer> res = new ArrayList<>();
         res.add(1);
         res.add(2);
         res.add(3);
         res.add(4);
         res.add(5);
-
         // 指定的节点的头部来进行添加内容
         // 由于内部是基于数组来实现的，所以可以随机位置的访问和添加元素。
-        res.add(0, 0);
+        res.addFirst(0);
 
         res.forEach(System.out::println);
-
         System.out.println("-------------------------------");
         // 作为链表的结构。
         Integer[] li = {1, 2, 3, 4, 6, 8, 9, 10};
