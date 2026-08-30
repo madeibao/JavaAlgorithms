e316cf52e89bbe2bbadf4e4419f249460a1969c0
madeibao <2901429479@qq.com>
2026-08-30 16:52:11 +0800

    add some demo

--- src/com/hashMap/www/HashMapValue2.java
+++ src/com/hashMap/www/HashMapValue2.java
@@ -15,32 +15,32 @@ public class HashMapValue2 {
 
         // Map按value排序
         HashMap<Character, Integer> map = new HashMap<>();
-        String str = "xyxyXX";
+        String str = "xyxyXXX";
         for (var i : str.toCharArray()) {
             map.put(i, map.getOrDefault(i, 0) + 1);
         }
-
-        List<Map.Entry<Character, Integer>> list = new ArrayList<>(map.entrySet());
-        Collections.sort(list, new Comparator<Map.Entry<Character, Integer>>() {
-            @Override
-            public int compare(Map.Entry<Character, Integer> o1, Map.Entry<Character, Integer> o2) {
-                /** return o2.getValue()-o1.getValue(); */
-                if (!Objects.equals(o1.getValue(), o2.getValue())) {
-                    return o2.getValue() - o1.getValue();
-                } else if (o1.getKey().equals(o2.getKey())) {
-                    return o1.getKey() - o2.getKey();
-                }
-                return 0;
-            }
-        });
-
-        String res = "";
+        List<Map.Entry<Character, Integer>> list = getEntries(map);
+        StringBuilder res = new StringBuilder();
         for (var j : list) {
             System.out.println(j.getKey() + " " + j.getValue());
         }
         for (var k : list) {
-            res += k.getKey() + ":" + k.getValue() + ";";
+            res.append(k.getKey()).append(":").append(k.getValue()).append(";");
         }
         System.out.println(res);
+    }
+
+    private static List<Map.Entry<Character, Integer>> getEntries(HashMap<Character, Integer> map) {
+        List<Map.Entry<Character, Integer>> list = new ArrayList<>(map.entrySet());
+        list.sort((o1, o2) -> {
+            /** return o2.getValue()-o1.getValue(); */
+            if (!Objects.equals(o1.getValue(), o2.getValue())) {
+                return o2.getValue() - o1.getValue();
+            } else if (!Objects.equals(o2.getKey(), o1.getKey())) {
+                return o1.getKey() - o2.getKey();
+            }
+            return 0;
+        });
+        return list;
     }
 }
