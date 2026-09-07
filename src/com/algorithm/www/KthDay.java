45fef1202a0193d1e87a54c8487cbdd3122ca15e
madeibao <2901429479@qq.com>
2026-09-07 21:49:37 +0800

    fix some problem

--- src/com/algorithm/www/KthDay.java
+++ src/com/algorithm/www/KthDay.java
@@ -9,23 +9,21 @@ package com.algorithm.www;
  *
  * 判断一年之中的第多少天。
  **/
-
 public class KthDay {
     public static void change(String str) {
-        int[] months={31,28,31,30,31,30,31,31,30,31,30,31};
+        int[] months = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
         System.out.println(str);
-        String[] sourceStrString=str.split("\\.");
-        int year=Integer.parseInt(sourceStrString[0]);
-        int month=Integer.parseInt(sourceStrString[1]);
-        int day=Integer.parseInt(sourceStrString[2]);
-
+        String[] sourceStrString = str.split("\\.");
+        int year = Integer.parseInt(sourceStrString[0]);
+        int month = Integer.parseInt(sourceStrString[1]);
+        int day = Integer.parseInt(sourceStrString[2]);
         // 判断闰年
-        months[1]=(year%400==0||year%4==0&&year%100!=0) ? 29:28;
-        int num=0;
-        for(int i=1;i<month;i++) {
-            num+=months[i];
+        months[1] = (year % 400 == 0 || year % 4 == 0 && year % 100 != 0) ? 29 : 28;
+        int num = 0;
+        for (int i = 1; i < month; i++) {
+            num += months[i];
         }
-        System.out.println(num+day);
+        System.out.println(num + day);
     }
 
     public static void main(String[] args) {
